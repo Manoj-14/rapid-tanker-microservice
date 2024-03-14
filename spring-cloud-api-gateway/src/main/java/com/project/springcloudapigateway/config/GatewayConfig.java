2bed6ac5d3100b20e644272abe38eff676dc988e
Manoj M <manumadhu1425@gmail.com>
2024-03-14 15:43:35 +0530

    api gateway added

--- spring-cloud-api-gateway/src/main/java/com/project/springcloudapigateway/config/GatewayConfig.java
+++ spring-cloud-api-gateway/src/main/java/com/project/springcloudapigateway/config/GatewayConfig.java
@@ -1,2 +1,20 @@
-package com.project.springcloudapigateway.config;public class GatewayConfig {
+package com.project.springcloudapigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator gateWayRouter(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route(p-> p.path("/api/supplier/**")
+                        .uri("lb://water-supplier-service"))
+                .route(p-> p.path("/api/tanker-supplier/**")
+                        .uri("lb://tanker-service"))
+                .build();
+    }
 }
